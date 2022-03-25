5655d5599c0837399f7c7b7e77b351af6cff56c3
Floris <florisvanrossum@gmail.com>
2022-03-24 23:50:32 -0400

    created some parquet files

--- src/main/scala/com/floris/App.scala
+++ src/main/scala/com/floris/App.scala
@@ -1,15 +1,29 @@
 package com.floris
 
+import org.apache.spark.sql.SparkSession
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
   def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    // Create spark session
+    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
+    println("Successfully started SparkSession")
+
+    val jsonFile = "/home/floris/IdeaProjects/scala-test-again/playground-project/data/all_scripts_raw.json" // Should be some file on your system
+    val csvFile = "/home/floris/IdeaProjects/scala-test-again/playground-project/data/artists.csv" // Should be some file on your system
+
+    //val logData = spark.read.textFile(logFile).cache()
+    val df = spark.read.csv(csvFile)
+
+    df.show()
+
+    df.write.parquet("/home/floris/IdeaProjects/scala-test-again/playground-project/data/output.parquet")
+
+    println(s"Finished.")
+    spark.stop()
   }
 
 }
